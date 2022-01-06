b8c2030be4a9fdf090bc8629946bb8237c044d50
Richard Neumann <r.neumann@homeinfo.de>
2022-01-06 15:04:18 +0100

    Add setup.py and Makefile

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+"""Installation script."""
+
+from setuptools import setup
+
+
+setup(
+    name='typingplus',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info at homeinfo dot de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r dot neumann at homeinfo priod de>',
+    py_modules=['typingplus'],
+    license='GPLv3',
+    description='Enhanced type hinting hacks.'
+)
